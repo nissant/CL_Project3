0d822d90c7c94ef33ea658d1f48eac9d7630aaae
Nisan <tnisan@gmail.com>
2019-05-14 18:23:14 +0300

    Update ex3_lb.c
    Some design changes to lb

--- ex3_lb.c
+++ ex3_lb.c
@@ -7,25 +7,25 @@
 
 // Function designed for chat between client and server. 
 
-void load_balance_servers() 
+void load_balance_traffic() 
 { 
-    char msg_buffer[BUFFER_INIT_SIZE];
+	char buffer[BUFFER_INIT_SIZE];	// char response_buffer[BUFFER_INIT_SIZE];
 	int buffer_size = BUFFER_INIT_SIZE;
 	int next_server_index = 0;
     // infinite loop for chat 
-    while (1) { 
-        bzero(msg_buffer, BUFFER_INIT_SIZE);
-        
-		get_client_request(connfd_client, msg_buffer, &buffer_size);	// read the message from client and copy it in buffer 
+    	while (1) { 
+		bzero(buffer, buffer_size);
+		// socket, msg type enum, buffer, buffer size
+		recv_msg(connfd_client, GET,&buffer, &buffer_size);	// read the message from client and copy it in buffer 
 
-        printf("From client to L.B:\n %s ", msg_buffer);
-		
-		if (get_server_response(next_server_index, msg_buffer, buffer_size) == 0) {
+		printf("From client to L.B:\n %s ", msg_buffer);
+
+		if (get_server_response(next_server_index, &msg_buffer, &buffer_size) == 0) {
 			printf("Server %d disconnected! \n", next_server_index);
 			return;
 		}
 
-		send_client_response(msg_buffer, buffer_size);
+		send_msg(connfd_client,msg_buffer, buffer_size);
 		next_server_index++;
 		if (next_server_index == SERVER_COUNT) { next_server_index = 0; }
 		wait_new_client();
@@ -36,7 +36,7 @@ void load_balance_servers()
 int main() { 
 	srandom(time(0));		// Use current time as seed for random generator
 	connect_servers_client();
-	load_balance_servers();
-    return 0;
+	load_balance_traffic();
+    	return 0;
 } 
 
